ce83b9b30519db7a57c39e35c09dabd70b110d39
Mark Thekkethala <mthekkethala@login-students.ccs.neu.edu>
2020-01-24 14:58:23 -0500

    organized into project folder

---
+++ Assignment1/client.py
@@ -0,0 +1,55 @@
+import socket
+
+neu_id = '001880538'
+host_ip = 'fring.ccs.neu.edu'
+port = 27993
+tup1 = (host_ip, port)
+
+# create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+## Connect to an IP with Port, could be a URL
+sock.connect(tup1)
+
+## Send some data, this method can be called multiple times
+m1 = 'cs3700spring2020 HELLO ' + neu_id + '\n'
+sock.sendall(m1.encode('ascii'))
+
+from_server = ''
+continue_recv = True
+find_continue_recv = True
+
+while find_continue_recv:
+  ##### GET SERVER MESSAGE ######
+  while continue_recv:
+    ## Receive up to 4096 bytes from a peer
+    curMessage = sock.recv(8192)
+    from_server += curMessage.decode('ascii')
+    newline_count = from_server.count("\n", 0, len(from_server))
+    if newline_count > 0:
+      continue_recv = False
+
+  # If server responds with another find message reply with count
+  find_count = from_server.count("FIND",0,len(from_server)) 
+  if find_count > 0:
+    ##### SEND COUNT MESSAGE ######
+    key = from_server[22]
+    key_count = from_server.count(key, 23, len(from_server))
+    m2 = 'cs3700spring2020 COUNT ' + str(key_count) + '\n'
+    sock.sendall(m2.encode('ascii'))
+    m2 = ''
+    m1 = ''
+    from_server = ''
+    continue_recv = True
+  # If server responds with BYE message print secret key
+  else: 
+    bye_count = from_server.count("BYE",0,len(from_server))
+    if  bye_count > 0:
+      if from_server.split(' ')[2] == 'Unknown_Husky_ID':
+        print("Unknown_Husky_ID")
+      else:
+        print(from_server.split(' ')[2].replace('\n',''))
+      find_continue_recv = False;
+
+## Close the socket connection, no more data transmission
+sock.close()
